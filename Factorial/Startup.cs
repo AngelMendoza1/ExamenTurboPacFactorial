4c0499a2d7f1aa53f6e1a80fdf7fb6b45b84433d
AngelMendoza1 <89880077+AngelMendoza1@users.noreply.github.com>
2021-09-02 15:00:39 -0500

    Examen

---
+++ Factorial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Factorial.Startup))]
+namespace Factorial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ FactorialEjercicio/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialEjercicio
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int op;
+            string num;
+            do{
+                Console.WriteLine("1-hacer el factorial\n2-Salir");
+                op=int.Parse(Console.ReadLine());
+                if(op==1){
+                    Console.WriteLine("Ingresa el numero");
+                    num = Console.ReadLine();
+                    if (valida(num))
+                    {
+                        Console.WriteLine(factorial(int.Parse(num)));
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("No es una opcion valida");
+                    }
+                }
+            }while(op!=2);
+        }
+
+
+        public static int factorial(int n)
+        {
+            if (n == 1)
+
+                return 1;
+
+            return n * factorial(n - 1); 
+
+        }
+        public static bool valida(string cadena)
+        {
+            try
+            {
+            int val = int.Parse(cadena);
+            return true;
+        }
+            catch(Exception exe){
+                return false;
+            }
+        }
+
+    }
+}
